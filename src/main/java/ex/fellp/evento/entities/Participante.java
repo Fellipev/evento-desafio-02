559c2e2e35eae68bf45488d3e31aba8a1b535d4e
Fellipev <fellipe2reis@gmail.com>
2025-09-02 13:50:29 -0300

    Get and Setter participante

--- src/main/java/ex/fellp/evento/entities/Participante.java
+++ src/main/java/ex/fellp/evento/entities/Participante.java
@@ -19,4 +19,42 @@ public class Participante {
 
     public Participante() {}
 
+    public Participante(Integer id, String nome, String email, Set<Atividades> atividades) {
+        this.id = id;
+        this.nome = nome;
+        this.email = email;
+        this.atividades = atividades;
+    }
+
+    public Set<Atividades> getAtividades() {
+        return atividades;
+    }
+
+    public void setAtividades(Atividades atividade) {
+        this.atividades.add(atividade);
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        this.nome = nome;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
 }
